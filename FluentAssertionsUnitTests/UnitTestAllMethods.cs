3b7c0b46aa1a7b737cbcb0b42054ab18fbbe8518
Freddy Juhel <domaine75@gmail.com>
2017-03-04 23:33:46 +0100

    Add Fluent assertions unit test project

---
+++ FluentAssertionsUnitTests/UnitTestAllMethods.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace FluentAssertionsUnitTests
+{
+  [TestClass]
+  public class UnitTestAllMethods
+  {
+    [TestCase]
+    public void Should_Start_With_Example()
+    {
+      const string actual = "ABCDEFGHI";
+      actual.Should().StartWith("AB").And.EndWith("HI").And.Contain("EF").And.HaveLength(9);
+    }
+  }
+}
