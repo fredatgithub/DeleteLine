e16255f7ce222da30b76da3a981fc6a11ab6e05d
Freddy Juhel <domaine75@gmail.com>
2017-04-26 23:10:01 +0200

    increase version number and public methods for unit tests

--- DeleteLine/Program.cs
+++ DeleteLine/Program.cs
@@ -517,7 +517,7 @@ namespace DeleteLine
     /// </summary>
     /// <param name="filename">The initial string to be processed.</param>
     /// <returns>A string without Windows forbidden characters.</returns>
-    private static string RemoveWindowsForbiddenCharacters(string filename)
+    public static string RemoveWindowsForbiddenCharacters(string filename)
     {
       string result = filename;
       // We remove all characters which are forbidden for a Windows path
@@ -535,7 +535,7 @@ namespace DeleteLine
     /// </summary>
     /// <param name="fileName">The name of the file.</param>
     /// <returns>A string with the date at the end of the file name.</returns>
-    private static string AddDateToFileName(string fileName)
+    public static string AddDateToFileName(string fileName)
     {
       string result = string.Empty;
       // We strip the fileName and add a datetime before the extension of the filename.
